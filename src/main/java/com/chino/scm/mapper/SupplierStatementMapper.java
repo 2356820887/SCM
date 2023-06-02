1939d069b2fb35ad93837152c503ca402cad601e
Chino <q1q1q11q1q1q11@163.com>
2023-06-02 09:42:03 +0800

    scm

--- src/main/java/com/chino/scm/mapper/SupplierStatementMapper.java
+++ src/main/java/com/chino/scm/mapper/SupplierStatementMapper.java
@@ -2,11 +2,7 @@ package com.chino.scm.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.chino.scm.pojo.SupplierStatement;
-
-import java.util.List;
-
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
