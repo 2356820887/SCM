45b01d5a69fa0dabb417d3e5682584ca1c48b569
Chino <q1q1q11q1q1q11@163.com>
2023-06-09 22:07:48 +0800

    SupplierController添加错误消息

--- src/main/java/com/chino/scm/controller/SupplierController.java
+++ src/main/java/com/chino/scm/controller/SupplierController.java
@@ -18,28 +18,48 @@ public class SupplierController {
 
     Map<String, Object> data = new HashMap<>();
 
-    @GetMapping("find-all")
+    @GetMapping("/find-all")
     public Result<Map<String, Object>> findAll() {
-        data.put("data", supplierService.findAll());
-        return new Result<>(200, data, "成功");
+        try {
+            data.put("data", supplierService.findAll());
+            return new Result<>(200, data, "成功");
+        } catch (Exception e) {
+            //在控制台打印异常信息，方便调试
+            e.printStackTrace();
+            return new Result<>(500, null, "服务器错误：查询供应商信息失败");
+        }
     }
 
     @GetMapping("/find-name-cname")
     public Result<Map<String, Object>> findByNameOrContactName(@RequestParam(value = "name", required = false) String name, @RequestParam(value = "contactName", required = false) String contactName) {
-        data.put("data", supplierService.findByNameOrContactName(name, contactName));
-        return new Result<>(200, data, "成功");
+        try {
+            data.put("data", supplierService.findByNameOrContactName(name, contactName));
+            return new Result<>(200, data, "成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Result<>(500, null, "服务器错误：按名称或联系人名称查询供应商信息失败");
+        }
     }
 
     @PostMapping("/update")
     public Result<Map<String, Object>> updateSupplier(@RequestBody Supplier supplier) {
-        data.put("data", supplierService.updateSupplier(supplier));
-        return new Result<>(200, data, "成功");
+        try {
+            data.put("data", supplierService.updateSupplier(supplier));
+            return new Result<>(200, data, "成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Result<>(500, null, "服务器错误：更新供应商信息失败");
+        }
     }
 
     @PostMapping("/add")
     public Result<Map<String, Object>> addSupplier(@RequestBody Supplier supplier) {
-        System.out.println(supplier);
-        data.put("data", supplierService.addSupplier(supplier));
-        return new Result<>(200, data, "成功");
+        try {
+            data.put("data", supplierService.addSupplier(supplier));
+            return new Result<>(200, data, "成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Result<>(500, null, "服务器错误：添加供应商信息失败");
+        }
     }
 }
